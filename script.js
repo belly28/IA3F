581f925e69995a1eba38d43dfcdc7b3d21f7fd63
belly28 <163352757+belly28@users.noreply.github.com>
2024-08-15 16:51:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
-const caixaAlternativas = document.querySelector('.caixa-alternativas')
-const caixaResultado = document.querySelector('.caixa-resultado')
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
 
-const perguntas = [
-    {
+const perguntas = [ //serve para abrir lista de perguntas
+    {//abre o objeto das perguntas
         enunciado: "Pergunta 1",
-        alternativas:[
-            "Alternativa 1",
-            "Alternativa 2"
-        ]
+        alternativas: ["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["Alternativa 1","Alternativa 2"]
     }
+    
+    enunciado: "Pergunta 3",
+    alternativas: ["Alternativa 1","Alternativa 2"]
 ]
+
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+        perguntaAtual = perguntas [atual];
+        caixaPerguntas.textContent = perguntaAtual
+}
